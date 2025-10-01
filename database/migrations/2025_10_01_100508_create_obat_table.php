6e47108043e6be5230dcca6e058a25c2b535f4b5
dwiprayoga10 <hazard.prayoga110303@gmail.com>
2025-10-01 17:44:39 +0700

    Tugas:1

---
+++ database/migrations/2025_10_01_100508_create_obat_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::create('obat', function (Blueprint $table) {
+        $table->id();
+        $table->string('nama_obat', 100);
+        $table->string('kemasan', 35)->nullable();
+        $table->unsignedInteger('harga');
+        $table->timestamps();
+    });
+}
+
+public function down(): void
+{
+    Schema::dropIfExists('obat');
+}
+
+};
